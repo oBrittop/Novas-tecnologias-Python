4c988091fa3088c2a4ee6f3f060d567fcb2c1fa7
oBrittop <gustavobrito2416@gmail.com>
2026-05-04 11:55:23 -0300

    feat: desafio 2 pandas concluido

--- n2/pandas_titanic/desafio2.py
+++ n2/pandas_titanic/desafio2.py
@@ -7,5 +7,22 @@ df = pd.read_csv(
     na_values=["N/A","-"]
 )
 print(df.isnull().sum())
-df["salario"].dropna(inplace=True)
 print(df)
+print("-"*150)
+df.dropna(subset=["salario"], inplace=True)
+print(df)
+print("-"*150)
+media_idade = df["idade"].mean()
+df.fillna({"idade":media_idade}, inplace=True)
+print(df)
+print("-"*150)
+df.info()
+df["data_admissao"] = pd.to_datetime(df["data_admissao"])
+dias_trabalhados = (pd.to_datetime("today") - df["data_admissao"]).dt.days
+df["anos_empresa"] = (dias_trabalhados/365.25).astype(float)
+print(df)
+print("-"*150)
+media_departamento = df.groupby("departamento")["salario"].transform("mean")
+filtro_anos_empresa = df["anos_empresa"] > 5
+filtro_salario_baixo = df["salario"] < media_departamento
+print(df[filtro_salario_baixo & filtro_anos_empresa])

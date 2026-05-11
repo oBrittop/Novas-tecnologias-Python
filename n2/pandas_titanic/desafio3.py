ba4410fdb02f3eef6584eb303d9848343e5c49fe
oBrittop <gustavobrito2416@gmail.com>
2026-05-11 18:24:23 -0300

    feat desafio 3 80% feito

--- n2/pandas_titanic/desafio3.py
+++ n2/pandas_titanic/desafio3.py
@@ -18,32 +18,32 @@ df_vendas = pd.read_csv(
     encoding= "utf-8",
     na_values=["N/A","-"]
 )
-df_aux = pd.merge(df_estoque, df_produtos, on="produto_id", how='left')
+df_aux_custo_total_estoque = pd.merge(df_estoque, df_produtos, on="produto_id", how='left')
 # df = pd.merge(df_aux, df_vendas, on="produto_id", how="inner")
 print(df_estoque.head())
 print("-"*120)
 print(df_produtos.head())
 print("-"*120)
 print(df_vendas.head())
-print(df_aux.head())
-custo_total_estoque = df_aux.groupby("quantidade")["preco_custo"].prod()
-df_aux["custo_total_estoque"] = custo_total_estoque
-print(custo_total_estoque)
-print(df_aux.head())
+print("-"*120)
+print(df_aux_custo_total_estoque.head())
+print("-"*120)
+df_aux_custo_total_estoque["custo_total_estoque"] = df_aux_custo_total_estoque["quantidade"] * df_aux_custo_total_estoque["preco_custo"]
+print(df_aux_custo_total_estoque.head())
+print("-"*120)
+df_aux_valor_venda_mes = pd.merge(df_vendas, df_produtos, on="produto_id", how="left")
+markup = 1.5
+df_aux_valor_venda_mes["valor_venda_mes"] = df_aux_valor_venda_mes["quantidade_vendida"] * df_aux_valor_venda_mes["preco_custo"] * markup
+print(df_aux_valor_venda_mes)
+print("-"*120)
 
-
-
-
-
-
-
-
-
-
-
-
-# df["custo_total"] = (df["quantidade"] * df["preco_custo"])
-# df["valor_venda_mes"] = (df["quantidade_vendida"] * df["preco_custo"] * 1.5)
-# print(df)
-# filtro_estoque_insuficiente = df["quantidade_vendida"] > df["quantidade"]
-# print(df[filtro_estoque_insuficiente])
+print("Filtro df estoque baixo:")
+df_baixo_estoque = pd.merge(df_vendas, df_estoque, on="produto_id", how="right")
+print(df_baixo_estoque)
+print("-"*120)
+filtro_sem_estoque = df_baixo_estoque["quantidade_vendida"] > df_baixo_estoque["quantidade"]
+df_baixo_estoque_filtraqdo =df_baixo_estoque[filtro_sem_estoque]
+if df_baixo_estoque_filtraqdo.empty:
+    print("Nao tem nenhum registro de estoque negativo!")
+else:
+    print(df_baixo_estoque_filtraqdo)
